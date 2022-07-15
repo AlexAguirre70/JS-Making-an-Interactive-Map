9aeabc1d94fabba723e04f037e39f193d5fe5c64
Alex Aguirre <teamaguirre@gmail.com>
2022-07-14 19:49:21 -0700

    Created Fetch command from Foursquare and parsed the data

--- mapapp.js
+++ mapapp.js
@@ -1,6 +1,7 @@
 //Create variables to set latitude and longitude coordinates
 let userLat=0
 let userLong=0
+
 // Get user Lat and Long coordinates to pass to makeMap function
 async function getCoords(){
 	const pos = await new Promise((resolve, reject) => {
@@ -47,11 +48,20 @@ async function searchFS (userLat, userLong,biztype){
             Authorization:'fsq3EK8/6H6eGJXVoq7oMhzlLEv6fvJC7tDkmelJpUBORCI='
         }
     }  
-    let response= await fetch(`https://api.foursquare.com/v3/places/search?query=${biztype}&ll=${userLat}%2C${userLong}&radius=8048`, options)
-    .then(response => response.json())
+    let response= await fetch(`https://api.foursquare.com/v3/places/search?query=${biztype}&ll=${userLat}%2C${userLong}&radius=8048&limit=5`, options)
+    let data = await response.text();
+    let parsedData = JSON.parse(data);
+    let bizresults = parsedData.results
     
+    // Need to pass these business results array of objects to a function
+    return bizresults          
 }
+/* Watch video on how to process parsed result and iterate through the objects */
 
+/* Function to procss the search results. 
+It should go through each object. grab the name, lat and long and place it on the map with a marker
+use the four square APi to read through the data results
+*/
 // Create map
 getCoords()
 
